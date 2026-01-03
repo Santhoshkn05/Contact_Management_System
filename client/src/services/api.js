af300b7f1f5ec5aa55a98ad0f7d16e8d34e6cf9e
santhosh <santhosh@gmail.com>
2026-01-03 09:46:16 +0530

    Finalize API service

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,20 +1,44 @@
 const BASE_URL = "/api/contacts";
 
+/**
+ * Fetch all contacts
+ */
 export const getContacts = async () => {
   const res = await fetch(BASE_URL);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch contacts");
+  }
+
   return res.json();
 };
 
+/**
+ * Add a new contact
+ */
 export const addContact = async (data) => {
-  await fetch(BASE_URL, {
+  const res = await fetch(BASE_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to add contact");
+  }
 };
 
+/**
+ * Delete a contact by ID
+ */
 export const deleteContact = async (id) => {
-  await fetch(`${BASE_URL}/${id}`, {
+  const res = await fetch(`${BASE_URL}/${id}`, {
     method: "DELETE",
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to delete contact");
+  }
 };
